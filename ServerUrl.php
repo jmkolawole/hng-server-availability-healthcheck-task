62ab6f365fd545406b1a61a053c426cba33d6341
korlahwarleh <jmkolawole@gmail.com>
2020-06-10 13:42:31 +0100

    Made more correction and added a new function

--- ServerUrl.php
+++ ServerUrl.php
@@ -1,33 +1,80 @@
 <?php
-class ServerUrl{
-    //Function that tests the connections
-    public function testConnection($server,$username,$password){
+
+class ServerUrl
+{
+    public $server;
+    public $username;
+    public $password;
+    public $port;
+    public $timeout;
+
+    public function __construct($server, $username, $password,$port=null,$timeout=null)
+    {
+        //Instantiate the class
+        $this->server = $server;
+        $this->username = $username;
+        $this->password = $password;
+        $this->port = $port;
+        $this->timeout = $timeout;
+    }
+
+    public function testServerConnection()
+    {
         $timestamp = time();
-        $link = new mysqli($server,$username,$password);
-        if($link->connect_errno){
+        $link = new mysqli($this->server, $this->username, $this->password);
+        if ($link->connect_errno) {
             $output = "";
             http_response_code(503);
             $output .= "Server URL: ";
-            $output .= $server;
+            $output .= $this->server;
             $output .= "\n";
             $output .= "Server Status: Unable to connect - Unavailable";
             $output .= "\n";
             $output .= "Timestamp: ";
-            $output .= date('F d, Y h:i:s A',$timestamp);
+            $output .= date('F d, Y h:i:s A', $timestamp);
+            $output .= "\n";
 
             return $output;
-        }else{
+        }else {
             $output = "";
             http_response_code(200);
             $output .= "Server URL: ";
-            $output .= $server;
+            $output .= $this->server;
             $output .= "\n";
             $output .= "Server Status: Connection Successful - Available";
             $output .= "\n";
             $output .= "Timestamp: ";
-            $output .= date('F d, Y h:i:s A',$timestamp);
+            $output .= date('F d, Y h:i:s A', $timestamp);
+            $output .= "\n";
+            $output .= "\n";
 
             return $output;
+        }
+
+    }
+
+    public function ping(){
+        
+        $tb = microtime(true);
+        $fp = fsockopen($this->server,$this->port,$errno,$errstr, $this->timeout);
+        if(!$fp){
+            $output = "";
+            $output .= $errstr;
+
+            return $output;
+        }else{
+            $output = "";
+            
+            $output .= "Pinged";
+
+
+            $ta = microtime(true);
+            $output .= " In ".(($ta - $tb) * 1000).'ms';
+
+
+
+            return $output;
+
         }
     }
 }
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 
 <?php
 
-
 //This function helps to autoload classes
 spl_autoload_register(function ($class){
     require_once $_SERVER['DOCUMENT_ROOT'].'/hng-server-availability-healthcheck-task/'.$class.'.php';
@@ -11,6 +10,10 @@ spl_autoload_register(function ($class){
 header('Content-type: application/json');
 
 //Creating an instance of the class
-$test = new ServerUrl();
-$result = $test->testConnection('127.0.0.1:3306','root','');
+$test = new ServerUrl('127.0.0.1','root','',3306,10); // The last two arguments are optional unless you want to ping the server
+$result = $test->testServerConnection(); //Test the connection
 echo $result;
+$result2 = $test->ping(); //Ping the Server
+echo $result2;
+
+
